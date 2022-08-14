6e711a472dc7b3438d97383692a3a5223b92d424
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-08-14 16:23:38 +0500

    001

---
+++ EX001/Program.cs
@@ -0,0 +1,18 @@
+﻿ising System.Ling;
+string text = "(1,2) (2,3) (4,5) (6,7)"
+               .Replace("(","")
+               .Replace(")","")
+               ;
+System.Console.WriteLine(text);
+
+var date = text.Split(" ");
+                .Select(item => item.Split(','));
+                .Select(e=> (x: int.Parse(e([0]), y: int.Parse(e[1]))));
+                .Where(e=> e.x%2==0);
+                .Select(point=> (point.x*10, point.y));
+                .ToArray();
+for (int i = 0; i < data.Length; i++)
+{
+    System.Console.WriteLine(data[i]);
+}
+                
